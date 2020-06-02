a7a4af3a4d38f7d56295900878c8ab6f740f644e
Lex Ortiz <lex.ortizc@gmail.com>
2020-06-02 01:37:46 -0600

    Login terminado

--- main.c
+++ main.c
@@ -1,61 +1,209 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <conio.h>
 #include <string.h>
 #include <locale.h>
-#include <ctype.h>
+// #include <ctype.h>
+// #include <conio.h>
 
-// Creando apodos para las variables a utilizar en el nodo Login
-typedef char USER[25];
+// Creando variables personalizadas para el nodoUsuario
+typedef char NAME[25];
 typedef char PASSWORD[80];
 typedef int ROLE; // 1: local, 2: admin
 
-// Creando la estructura nodoLogin
-typedef struct nodoLogin{
-	USER usuario;
+// Creando la estructura nodoUser
+typedef struct nodoUser{
+	NAME nombre;
 	PASSWORD clave;
 	ROLE role;
-	struct LOGIN *siguiente;
-	struct LOGIN *anterior;
-}LOGIN;
+	struct nodoUser *siguiente;
+} USER;
 
 // Creando punteros tipo login para la lista enlazada
-LOGIN *primero = NULL;
-LOGIN *ultimo = NULL;
-LOGIN *usuario = NULL;
+USER *primero = NULL;
+USER *ultimo = NULL;
+USER *usuario = NULL;
 
-// Creando los prototipos de las funciones
-void leerUsuario(void);
+/* Creando los prototipos de las funciones */
+// Métodos de login
+void leerUsuarios(void);
+USER *agregarUsuario(NAME nombre, PASSWORD clave, ROLE role);
+void eliminarUsuario(NAME nombre);
+void mostrarUsuarios(void);
+void iniciarSesion(void);
+// Métodos de menu
 void mostrarMenu(int menu);
 int validarMenuInicio(char opc);
 int validarMenuLectura(char opc);
 int validarMenuAdmin(char opc);
 void opcionInvalida(void);
-void validarUsuario(void);
 
-// Método principal de la aplicación
+// Método principal del sistema
 int main() {
-	setlocale(LC_ALL,"Spanish");
+	setlocale(LC_ALL,"Spanish"); // Habilitando caracteres especiales
+	leerUsuarios(); // Leyendo los usuarios del sistema
 
-	/* DECLARACION DE VARIABLES */
+	// Declaración de variables
 	char opc; // Guardar la opc seleccionada del menú
 
-	/* LEYENDO LOS USUARIOS DEL SISTEMA */
-	leerUsuario();
-
-	/* INICIO DEL PROGRAMA... */
+	// Menú principal de la app
 	do {
+		usuario = NULL; // Inicializar usuario loggeado
 		mostrarMenu(1); // Mostrando el menu de principal
-		scanf("%c", &opc); // Capturando la entrada del teclado
+		scanf("\n%c", &opc); // Capturando la entrada del teclado
 	} while ( validarMenuInicio(opc) ); // Si se ingresa un valor invalido, validarMenu retornará TRUE
 
-	return 0;
+	return 0; // Finalizando el programa con éxito
 }
 
 // Método para agregar a una lista enlasada los usuario del sistema
-void leerUsuario(void) {
-	// leer archivo .csv
-	// Crear lista enlazada de usuarios
+void leerUsuarios(void) {
+	// Declaración de variables
+	USER *actual = NULL;
+	NAME nombre;
+	PASSWORD clave;
+	ROLE role;
+
+	FILE *fp = fopen("./files/login.txt", "r"); // Leyendo el archivo login.txt
+
+	if (fp == NULL) { // Si el archivo no se puede leer o abrir
+		perror(" Error al leer el archivo login.txt");
+	} else {
+		// Leyendo cada una de las lineas del archivo
+		while( feof(fp) == 0 ){ // Mientra no se llegue al final del archivo
+			int celda = 0;
+			char linea[500];
+			char split[] = ";";
+			fscanf(fp," %[^\n]", linea); // Tomar todos los caracteres de la linea leída
+			char *valor = strtok( linea, split); // Guardar valores separardos por ";"
+
+			// Recorrer todos los valores separados
+			while(valor != NULL){
+				switch(celda){ // Obtener cada valor repadado en la variable celda
+					case 0:
+						strcpy( nombre, valor ); // Copiando el valor obtenido para nombre
+						break;
+					case 1:
+						strcpy( clave, valor ); // Copiando el valor obtenido para clave
+						break;
+					case 2:
+						role = atoi( valor ); // Asignando el valor obtenido para role
+						break;
+				}
+				valor = strtok(NULL,split); // Limpiando el valor separado
+				celda++; // Avanzando al siguiente valor separado
+
+			}
+			// AGREGANDO USUARIO A LA LISTA ENLAZADA DE USUARIOS
+			if( primero == NULL ) { // Verficando si es el primer nodo de la lista
+				primero = agregarUsuario(nombre, clave, role); // Creando el primer nodo de la lista
+				ultimo = primero; // Creando el ultimo nodo de la lista
+			} else {
+				ultimo->siguiente = agregarUsuario(nombre, clave, role); // Agregar un nuevo nodo a la lista
+				ultimo = ultimo->siguiente; // Actualizando el ultimo nodo de la lista
+			}
+		}
+	}
+	fclose(fp); // Cerrando el archivo login.txt
+	mostrarUsuarios();
+}
+
+// Método para crear nuevos nodos tipo User
+USER *agregarUsuario(NAME nombre, PASSWORD clave, ROLE role) {
+  USER *nuevoUsuario; // Pointer de tipo USER
+  nuevoUsuario = malloc( sizeof(USER)); // Asignando espacio de memoria para el Pointer
+  nuevoUsuario->siguiente = NULL; // Evitar que el nodo siguiente apunte a cualquier lado
+  strcpy( nuevoUsuario->nombre, nombre );
+  strcpy( nuevoUsuario->clave, clave );
+  nuevoUsuario->role = role;
+
+  return nuevoUsuario; // Retornando el nuevo login
+}
+
+// Método para eliminar nodos de la lista usuarios
+void eliminarUsuario(NAME nombre) {
+	// logica para eliminar usuario
+}
+
+// Método para mostrar la lista de usuarios
+void mostrarUsuarios (void) {
+  USER *actual = primero; // actual toma el valor del nodo inicial
+	int cusuarios = 0; // Contador de usuarios
+
+  printf("\n * Lista de Usuarios * \n");
+  // Recorriendo toda la lista de nodos
+  while ( actual ) { // Mientras exista un nodo
+		cusuarios++; // Aumentando el contador
+		printf("\n-> USUARIO %d ", cusuarios);
+    printf("\nNombre: %s ", actual->nombre);
+		// printf("\nClave: %s ", actual->clave); // No se debe mostrar la clave
+		if( actual->role == 1) {
+			printf("\nRole: Usuario Local\n");
+		} else if ( actual->role == 2) {
+			printf("\nRole: Administrador\n");
+		}
+    actual = actual->siguiente; // actual toma el valor del siguiente nodo
+  }
+}
+
+// Método para iniciar sesión en el sistema
+void iniciarSesion(void) {
+	USER *actual = primero; // Obteniendo el primero nodo de la lista
+	NAME nombre; // Varible tipo NAME
+	PASSWORD clave; // Varible tipo PASSWORD
+	int encontrado = 0; // Bandera => 0: false, 1: true
+	char res; // Auxiliar para guardar respuestas del usuario
+
+  while ( encontrado == 0 ) {// Mientras no se encuentre el usuario
+		system("cls"); // Limpiando la pantalla
+
+		// Titulo de la pantalla
+		printf("\n|----------------------------------------|");
+		printf("\n|            Inicio de Sesión            |");
+		printf("\n|----------------------------------------|\n");
+
+		// Capturando datos del usuario para iniciar sesión
+		printf("\nIngrese el nombre de usuario: ");
+		scanf("%24s", nombre);
+		printf("Ingrese la contraseña: ");
+		scanf("%79s", clave);
+
+		// Recorrer la lista de usuario para verificar al usuario
+		while ( actual != NULL ) { // Mientras el nodo exista en la lista
+
+			// Comparando nombre y contraseña
+			if ( strcmp( actual->nombre, nombre ) == 0 ) { // Comparando el nombre
+				if ( strcmp( actual->clave, clave ) == 0 ) { //comparando la contraseña
+					encontrado = 1; // Aviso de usuario encontrado
+					usuario = actual; // Registrando al usuario loggeado
+
+					// Mostrando mensaje de Bienvenida
+					if ( usuario->role == 1) {
+						printf("\n Bienvenido %s (Usuario Local) \n", usuario->nombre);
+					} else if ( usuario->role == 2) {
+						printf("\n Bienvenido %s (Administrador) \n", usuario->nombre);
+					}
+					system("pause");
+				}
+			}
+
+			if ( encontrado == 0 ) { // Si no se ha encontrado el usuario
+				actual = actual->siguiente; // Pasando al siguiente nodo de la lista
+			} else if( encontrado == 1 ) { // Si ya se encontró el usuario
+				actual = NULL; // Terminando el ciclo while
+			}
+
+		}
+
+		if ( encontrado == 0 ) {
+			printf("\nCredenciales incorrectas. ¿Desea intentar de nuevo? [S/N]: ");
+			scanf("\n%c", &res);
+			if (res == 'n' || res == 'N'){
+				encontrado = 1; // Fingir un encontrado para Salir del while
+			} else {
+				actual = primero;
+			}
+		}
+	}
 }
 
 // Método para imprimir en pantalla los menus de la app
@@ -106,16 +254,15 @@ int validarMenuInicio(char opc) {
 	char res; // Auxiliar para guardar respuestas del usuario
 	int repetir = 1; // 0: False, 1: True
 
-
 	switch (opc) {
 		case '1':
 			do {
 				mostrarMenu(2); // Mostrar menú de lectura de archivos
-				scanf("%c", &res); // Capturando la entrada del teclado
+				scanf("\n%c", &res); // Capturando la entrada del teclado
 			} while ( validarMenuLectura(res) ); // Si se ingresa un valor invalido, validarMenu retornará TRUE
 			break;
 		case '2':
-			validarUsuario(); // Iniciar sesión
+			iniciarSesion(); // Iniciar sesión
 
 			// Si existe un usuario registrado, mostrar menú de opciones correspondiente
 			if ( usuario ) {
@@ -123,13 +270,13 @@ int validarMenuInicio(char opc) {
 				if( usuario->role == 1 ) { // Usuario Local
 					do {
 						mostrarMenu(2); // Mostrar menú de lectura de archivos
-						scanf("%c", &res); // Capturando la entrada del teclado
+						scanf("\n%c", &res); // Capturando la entrada del teclado
 					} while ( validarMenuLectura(res) ); // Si se ingresa un valor invalido, validarMenu retornará TRUE
 				}
 				if( usuario->role == 2 ) { // Usuario Administrador
 					do {
 						mostrarMenu(3); // Mostrar menú de administración de usuarios
-						scanf("%c", &res); // Capturando la entrada del teclado
+						scanf("\n%c", &res); // Capturando la entrada del teclado
 					} while ( validarMenuAdmin(res) ); // Si se ingresa un valor invalido, validarMenu retornará TRUE
 				}
 			}
@@ -147,13 +294,55 @@ int validarMenuInicio(char opc) {
 
 // Método para validar la opción seleccionada en el menú lectura
 int validarMenuLectura(char opc) {
-	int repetir = 0; // 0: False, 1: True
+	int repetir = 1; // 0: False, 1: True
+
+	// Verificando la opc selecionada
+	switch (opc) {
+		case '1':
+			// Busqueda general
+			break;
+		case '2':
+			// Busqueda personalizada
+			break;
+		case '3':
+			if( usuario ) { // Si existe un usuario local loggeado
+				// Mostrar Reportes
+			} else {
+				opcionInvalida(); // Mostrar msj de opción no disponible
+			}
+			break;
+		case '4':
+			repetir = 0;
+			break;
+		default:
+			opcionInvalida(); // Mostrar msj de opción no disponible
+			break;
+	}
+
 	return repetir; // Retorna 1 para retir y 0 para romper el ciclo
 }
 
 // Método para validar la opción seleccionada en el menú admin
 int validarMenuAdmin(char opc) {
-	int repetir = 0; // 0: False, 1: True
+	int repetir = 1; // 0: False, 1: True
+
+	// Verificando la opc selecionada
+	switch (opc) {
+		case '1':
+			// Busqueda general
+			break;
+		case '2':
+			// Busqueda personalizada
+			break;
+		case '3':
+			// Cerrar sesión
+			repetir = 0;
+			break;
+		default:
+			opcionInvalida(); // Mostrar msj de opción no disponible
+			break;
+	}
+
 	return repetir; // Retorna 1 para retir y 0 para romper el ciclo
 }
 
@@ -161,30 +350,6 @@ int validarMenuAdmin(char opc) {
 void opcionInvalida(void) {
 		printf("\nOpción no disponible. Por favor intente de nuevo\n");
 		system("pause"); // Hacer una pausa en el sistema
-}
-
-void validarUsuario(void) {
-	int encontrado = 0; // 0: false, 1: true
-	char res; // Auxiliar para guardar respuestas del usuario
-	printf("\nValidando usuario...");
-
-	// Se repite mientra encontrado == 0
-	while ( !encontrado ) {
-		// obtener usuario y contraseña
-		// Recorrer lista enlazada LOGIN
-		// Comparar usuario y contraseña
-		// Guardar usuario en el sistema (nombre, role)
-
-		// Si existe un usuario, activar bandera de encontrado
-		if ( usuario ) {
-			encontrado = 1;
-		} else {
-			printf("\nCredenciales incorrectas. ¿Desea intentar de nuevo? [S/N]: ");
-			scanf("%c", &res);
-			if (res == 'n' || res == 'N')
-			encontrado = 1; // Fingir un encontrado para Salir del while
-		}
-	}
 }
 
 // void limpiarPantalla(void)
